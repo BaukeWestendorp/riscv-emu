da5209186e1531edae0c86cf78b0c6608ddd33fd
Bauke Westendorp <baukewestendorp@gmail.com>
2025-04-29 15:54:47 +0200

    Add `clap` crate for parsing command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use cpu::Cpu;
 
 mod cpu;
@@ -7,6 +10,17 @@ mod reg;
 /// The amount of memory in the system in
 const MEMORY_SIZE: u64 = 1024 * 1024 * 128; // (128MiB)
 
+/// A RISC-V emulator.
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The RISC-V binary file to run.
+    #[arg(short, long)]
+    bin: PathBuf,
+}
+
 fn main() {
+    let args = Args::parse();
+
     Cpu::default().run();
 }
